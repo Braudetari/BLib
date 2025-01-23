c3c9e4cb58327d15f1c111ac772b4ec8d9269d44
Mor Hodaya Maman <Mor.Hodaya.Maman@e.braude.ac.il>
2025-01-24 00:42:47 +0200

    rebase prepare

--- BLibClient/src/gui/LoginFrameController.java
+++ BLibClient/src/gui/LoginFrameController.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.io.IOException;
+
 import client.ChatClient;
 import client.ClientUI;
 import common.*;
@@ -65,9 +67,25 @@ public class LoginFrameController extends Application {
 
     // Method to handle the "Enter as Guest" button action
     @FXML
-    private void getGuestBtn(ActionEvent event) {
-        labelmsg.setText("Logged in as guest.");
-        // Add logic to proceed as a guest (e.g., load a guest view)
+    private void getGuestBtn(ActionEvent event) throws Exception{
+    	Object[] values= ClientUI.chat.LoginToServer("Guest", "GUEST");
+    	String[] lr =ClientUI.chat.getClientLastResponses();
+    	if(lr[0].equals("error")) {
+            return;
+    	}
+    	else {
+    		String fullName= (String) values[1];
+    		User user= (User) values[0];
+    		try {
+        		(new MenuUIController()).start(user.getType(), fullName);
+        		((Stage)((Node)event.getSource()).getScene().getWindow()).close(); //close Frame
+    		}
+    		catch(IOException e) {
+    			e.printStackTrace();
+    			System.err.println("Could not start MenuUIController in getGuestBtn");
+    			System.exit(-1);
+    		}
+    	}
     }
 
     @Override
